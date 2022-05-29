c8c1070553b4975299aef98881df66da144ebec0
Lunga Vazi <Lunga.Vazi@bcx.co.za>
2022-05-30 00:11:17 +0200

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+var stack = new Stack();
+
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
---
+++ Inheritance/Stack.cs
@@ -0,0 +1,56 @@
+﻿
+namespace Inheritance
+{
+    public class Stack
+    {
+        private List<object> arrayListObj;
+
+        public Stack()
+        {
+            arrayListObj = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                        arrayListObj.Add(obj);
+                }
+                else { throw new InvalidProgramException("Can not pass a null object."); }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public object Pop()
+        {
+            try
+            {
+                if (arrayListObj != null && arrayListObj.Count > 0)
+                {
+                    var obj = arrayListObj[arrayListObj.Count - 1];
+                    arrayListObj.RemoveAt(arrayListObj.Count - 1);
+                    return obj;
+                }
+                else { throw new InvalidProgramException("Stack is emplty"); }
+
+            }
+            catch (Exception)
+            {
+                throw new InvalidProgramException("Stackk is emplty");
+            }
+        }
+
+        public void Clear()
+        {
+            arrayListObj.Clear();   
+        }
+
+    }
+}
